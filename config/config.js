8c127611a89c55e54c8330a4a42fec35582f178f
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-06-17 18:58:14 -0500

    Configurando .env para AWS

--- config/config.js
+++ config/config.js
@@ -15,14 +15,14 @@ process.env.PORT = process.env.PORT || 3000;
 // =========================================
 // Entorno
 // =========================================
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // =========================================
 // Base de Datos
 // =========================================
 let urlDB;
 
-if ( process.env.NODE_ENV === 'development' ) {
+if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/hospitalDB';
 } else {
     urlDB = process.env.MONGO_URI;
